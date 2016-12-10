839074c6323761e6cd3cce9d0d1d269cd33fa54d
Yigal Irani <yigal.irani@flexreceipts.com>
2016-12-10 11:55:58 -0500

    day8b.py

---
+++ day8b.py
@@ -0,0 +1,28 @@
+board=[[' ']*50 for x in range (6)]
+def print_board():
+	for x in board:
+		print '.'.join(x)
+
+def proccess_command(line):
+	def decode_command(begin,sep):
+		if line.startswith(begin):
+			yield map(int,line[len(begin):].split(sep))
+
+	for width,height in decode_command('rect ','x'):
+		for y in range(height):
+			for x in range(width):
+				board[y][x]='X'
+
+	for y,n in decode_command('rotate row y=',' by '):
+		row=board[y][:]
+		for x in range(50):
+			board[y][(x+n)%50]=row[x]
+
+	for x,n in decode_command('rotate column x=',' by '):
+		col=[board[y][x] for y in xrange (6)]
+		for y in range(6):
+			board[(y+n)%6][x]=col[y]
+
+for line in open('input8.txt').readlines():
+	proccess_command(line.strip())
+print_board()
