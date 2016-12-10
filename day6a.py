7459fc03c7ea8a943777b4c0bff24a7525eeeed9
Yigal Irani <yigal.irani@flexreceipts.com>
2016-12-10 01:10:20 -0500

    day6a.py

---
+++ day6a.py
@@ -0,0 +1,10 @@
+from collections import Counter
+d=[]
+for line in file('input6.txt').readlines():
+	for i,x in enumerate(line):
+		if (len(d)<=i):
+			d.append(Counter())
+		d[i][x]+=1
+def most_freq(x):
+	return max(x.iterkeys(), key=(lambda key: x[key]))
+print ''.join([most_freq(x) for x in d])
