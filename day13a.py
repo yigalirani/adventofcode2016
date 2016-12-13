d62e54516a0dcffdeb6573052d358cd5860cb0cf
Yigal Irani <yigal.irani@flexreceipts.com>
2016-12-13 00:25:24 -0500

    day 13

---
+++ day13a.py
@@ -0,0 +1,30 @@
+from collections import deque
+def graph_search(start_state,end_state,enum_neigboors):
+	visited=set()
+	visited.add(start_state)
+	queue=deque()
+	queue.append((start_state,0))
+	while(True):
+		current,cost=queue.popleft()
+		if (current==end_state):
+				return cost
+		for x in enum_neigboors(current):
+	
+			if (x not in visited):
+				visited.add(x)
+				queue.append((x,cost+1))
+
+def enum_next(x,y):
+	for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
+		if (x+dx>=0 and y+dy>=0):
+			yield (x+dx,y+dy)
+def is_open(x,y):
+	return bin(x*x + 3*x + 2*x*y + y + y*y+1350).count('1')%2==0
+
+def get_next(loc):
+	x,y=map(int,loc.split(','))
+	#print x,y
+	for x2,y2 in enum_next(x,y):
+		if is_open(x2,y2):
+			yield str(x2)+','+str(y2)	
+print graph_search('1,1','31,39',get_next)
