f940a6bf9ad66ade1a96b73791ecd0762419ada3
Yigal Irani <yigal.irani@flexreceipts.com>
2016-12-15 00:22:26 -0500

    day 15

---
+++ day15a.py
@@ -0,0 +1,28 @@
+#Disc #1 has 17 positions; at time=0, it is at position 5.
+import re
+from collections import defaultdict
+def yield_match(regex,input):
+	ans=re.search(regex,input)
+	if (ans):
+		yield ans.groups()
+
+disks=[]
+for line in open('input15.txt').readlines():
+	for disk,num_pos,start_pos in yield_match('Disc (.*) has (.*) positions; at time=0, it is at position (.*).',line):
+		disks.append((int(num_pos),int(start_pos)))
+
+def is_good_time(start_time):
+	for i,(num_pos,start_pos) in enumerate(disks):
+		if (start_time+i+start_pos+1)%num_pos:
+			return False
+	return True
+start_time=0
+while(True):
+	if (is_good_time(start_time)):
+		print start_time
+		exit()
+	start_time+=1
+
+	
+print disks
+		
