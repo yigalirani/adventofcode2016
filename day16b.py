4b2c53ba58584b0d1029a7b8e1631cb79a2a9983
Yigal Irani <yigal.irani@flexreceipts.com>
2016-12-16 03:44:41 -0500

    day 16

---
+++ day16b.py
@@ -0,0 +1,25 @@
+data='10011111011011001'
+disk_size=35651584
+
+def expand(a):
+	b=a.replace('1','a').replace('0','1').replace('a','0')[::-1]
+	return a+'0'+b
+
+def grouper(x):
+	it=iter(x)
+	for x in it:
+		yield x,next(it)
+	
+def checksum(a):
+	if len(a)%2==1:
+		return a
+	ans=''
+	for x,y in grouper(a):
+		ans+='1' if x==y else '0'
+	return checksum(ans)
+
+while(len(data)<disk_size):
+	data=expand(data)
+
+data=data[0:disk_size]
+print checksum(data)
